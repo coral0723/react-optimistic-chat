f006bebdbc5e3e967d5c6eee00ef93a14999c1b5
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-11-24 22:24:25 +0900

    feat(demo): ChatInputPlayGround props 예시 시나리오 구성
    - 다양한 props 조합 테스트

--- example/demo/src/pages/ChatInputPlayGround.tsx
+++ example/demo/src/pages/ChatInputPlayGround.tsx
@@ -1,7 +1,71 @@
+import { useState } from "react";
 import ChatInput from "../../../../src/components/ChatInput";
 
 export default function ChatInputPlayGround() {
+  const [text, setText] = useState<string>("");
+  const [value, setValue] = useState<string>("");
+
   return (
-    <ChatInput/>
+    <div className="max-w-4xl mx-auto flex flex-col">
+      <div className=" mb-4 p-2 border border-gray-500 whitespace-pre-wrap">
+        {text}
+      </div>
+      <div className="mb-2 w-full flex flex-col">
+        <span className="self-center text-xl font-bold mb-1">
+          {"<Default>"}
+        </span>
+        <ChatInput
+          onSend={(t: string) => {
+            setText(t);
+          }}
+        />
+      </div>
+      <div className="mb-2 w-full flex flex-col">
+        <span className="self-center text-xl font-bold mb-1">
+          {"<disableVoice>"}
+        </span>
+        <ChatInput
+          onSend={(t: string) => {
+            setText(t);
+          }}
+          disableVoice={true}
+        />
+      </div>
+      <div className="mb-2 w-full flex flex-col">
+        <span className="self-center text-xl font-bold mb-1">
+          {"<placeholder>"}
+        </span>
+        <ChatInput
+          onSend={(t: string) => {
+            setText(t);
+          }}
+          placeholder="무엇이든 물어보세요"
+        />
+      </div>
+      <div className="mb-2 w-full flex flex-col">
+        <span className="self-center text-xl font-bold mb-1">
+          {"<maxHeight=50>"}
+        </span>
+        <ChatInput
+          onSend={(t: string) => {
+            setText(t);
+          }}
+          maxHeight={50}
+        />
+      </div>
+      <div className="mb-2 w-full flex flex-col">
+        <span className="self-center text-xl font-bold mb-1">
+          {"<value & onChange>"}
+        </span>
+        <ChatInput
+          value={value}
+          onChange={(e) => setValue(e)}
+          onSend={(t: string) => {
+            setText(t);
+            setValue("");
+          }}
+        />
+      </div>
+    </div>
   )
 }
