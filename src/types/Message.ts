5fa7dc11c61d9733830396b196164e81f39b1ec4
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-12-22 14:02:40 +0900

    feat: Message 타입을 제네릭으로 변경

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,8 +1,10 @@
 export type ChatRole = "AI" | "USER";
 
-export type Message = {
+export type BaseMessage = {
   id: number | string;
   role: ChatRole;
   content: string;
   isLoading?: boolean;
 }
+
+export type Message<TExtra = {}> = BaseMessage & TExtra;
