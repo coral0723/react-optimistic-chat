51a1ebf78dc179b16672d678b17aa0354e82dde4
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2026-02-11 10:46:45 +0900

    docs: Message.ts 내부의 타입들마다 설명 추가

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,5 +1,6 @@
-export type ChatRole = "AI" | "USER";
+type ChatRole = "AI" | "USER";
 
+// 라이브러리 내부에서 불변으로 보장되는 최소 메시지 구조
 export type BaseMessage = {
   id: number | string;
   role: ChatRole;
@@ -7,9 +8,12 @@ export type BaseMessage = {
   isLoading?: boolean;
 }
 
-// map 함수에서 반드시 반환해야 하는 최소 필드
+// 사용자가 map 함수에서 반드시 반환해야 하는 최소 필드
+// 해당 타입이 깨지면 컴파일 타임에 바로 에러
 export type MessageCore = Pick<BaseMessage, "id" | "role" | "content">;
 
+// 최종적으로 UI / 상태에서 사용되는 메시지 타입
+// custom은 map 설계에서 의도적으로 소비되지 않은 Raw 필드 영역
 export type Message<TCustom = unknown> = BaseMessage & {
   custom?: TCustom;
 };
